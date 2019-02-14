f967eedba70002df932062fd73baaf0efe0cdc26
guillaumemmm <guillaume.meigniez@gmail.com>
2019-02-14 21:27:35 +0800

    hide api key

--- src/stores/EventsStore.js
+++ src/stores/EventsStore.js
@@ -15,13 +15,7 @@ class EventsStore {
 
     @action
     getEvents = () => {
-        axios.get('https://cors-anywhere.herokuapp.com/' + 'https://zpui5msqkg.execute-api.us-east-1.amazonaws.com/dev/events?category=veganism&lat=25&lon=121&sdate=1449000000&edate=1649290750', {
-            headers: {
-                'x-api-key': 'RV1LqrOcWg3dbZSmkPaNR1z4ZJDWKKMDwU5fmJ38'
-            }
-        })
-        .then(response => console.log(response.data))
-        .catch(err => console.log('err', err));
+        //  fetch data from api
     }
   }
   
